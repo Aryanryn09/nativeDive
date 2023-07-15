5754937410e906210a9c1ab5a4dbd00561307f46
Aryan Bansod <aryanem09@gmail.com>
2023-07-15 22:52:23 +0530

    exp 1

---
+++ AwesomeProject/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, StyleSheet, Text, useColorScheme } from "react-native";
+
+function App() {
+  const isDarkMode = useColorScheme() === "dark";
+
+  return (
+    <View style={styles.main}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+        main item
+      </Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  whiteText: {
+    color: "white",
+  },
+  darkText: {
+    color: "black",
+  },
+});
+
+export default App;
